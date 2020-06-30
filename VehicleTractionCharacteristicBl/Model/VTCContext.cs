5bab72fa8a8844b487e505665e9aab13b4d33aa1
Andrew Rybakow <AndrewRybakow@outlook.com>
2020-06-30 16:02:42 +0300

    Add Entity Framework to the project

---
+++ VehicleTractionCharacteristicBl/Model/VTCContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace VehicleTractionCharacteristicBl.Model
+{
+    class VTCContext : DbContext
+    {
+        public VTCContext() : base("VTCConnection") { }
+
+        public DbSet<Engine> Engine { get; set; }
+
+        public DbSet<Gear> Gears { get; set; }
+
+        public DbSet<Speed> Speeds { get; set; }
+
+        public DbSet<Characteristic> Characteristics { get; set; }
+    }
+}
